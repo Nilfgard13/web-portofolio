508168fabfeea57aa0afe131e166019020020c15
Nilfgard13 <fachrizalfazza@gmail.com>
2024-07-07 12:49:46 +0700

    Final Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+const hamburger = document.querySelector('#hamburger');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+});
